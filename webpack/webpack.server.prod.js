21b72c21ed80b6788a1db5d0d8eea88337432b7d
Thomas Aumaitre <thomas.aumaitre@gmail.com>
2021-11-17 19:10:15 +0100

    #patch heroku.yml v7

--- webpack/webpack.server.prod.js
+++ webpack/webpack.server.prod.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: paths.serverBuild,
     filename: 'server.js',
-    publicPath: paths.publicPath,
+    publicPath: `https://react-easy-ssr-staticfiles.herokuapp.com/${paths.publicPath}`,
     libraryTarget: 'commonjs2'
   },
   resolve: {
