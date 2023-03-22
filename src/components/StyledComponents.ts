180cbca16272206d1e5ede21a680b4ced4d1e7da
Thomas Aumaitre <thomas.aumaitre@gmail.com>
2023-03-22 08:44:23 +0100

    rewrite styled-components NotificationContainer

--- src/components/StyledComponents.ts
+++ src/components/StyledComponents.ts
@@ -1,42 +1,43 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import transition from '../assets/jss/theme/transition'
 import breakpoints from '../assets/jss/theme/breakpoints'
 
-export const NotificationContainer = styled.div(
-  (props: { type: 'notification' | 'block'; isOpen: boolean; index: number | undefined }) => `
-  ${
-    props.type === 'block'
-      ? `
-        flex: 1,
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 2rem;
-        `
-      : props.type === 'notification'
-      ? `
-          position: fixed;
-          zIndex: ${typeof props.index === 'number' ? 3000 + props.index : 3000};
-          zIndex: 3000;
-          bottom: -10rem;
-          right: 2rem;
-          ${breakpoints('down', 'sm')} {
-            right: 1rem;
-          }
-          transition: all 350ms ${transition.easing.easeInOut};
-          `
-      : ``
-  }
+interface NotificationContainerProps {
+  type: 'notification' | 'block'
+  isOpen: boolean
+  index: number | undefined
+}
 
-  ${
-    props.isOpen && typeof props.index === 'number'
-      ? `
-      bottom: ${3 + props.index * 6}rem;
+export const NotificationContainer = styled.div<NotificationContainerProps>`
+  ${(props) =>
+    props.type === 'block' &&
+    css`
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 2rem;
+    `}
+  ${(props) =>
+    props.type === 'notification' &&
+    css`
+      position: fixed;
+      zindex: ${typeof props.index === 'number' ? 3000 + props.index : 3000};
+      zindex: 3000;
+      bottom: -10rem;
+      right: 2rem;
       ${breakpoints('down', 'sm')} {
-        bottom: ${2 + props.index * 6}rem;
+        right: 1rem;
       }
-  `
-      : ``
-  }
-  `
-)
+      transition: all 350ms ${transition.easing.easeInOut};
+
+      ${props.isOpen &&
+      typeof props.index === 'number' &&
+      css`
+        bottom: ${3 + props.index * 6}rem;
+        ${breakpoints('down', 'sm')} {
+          bottom: ${2 + props.index * 6}rem;
+        }
+      `}
+    `}
+`
